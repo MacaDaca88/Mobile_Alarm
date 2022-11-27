06bcf9b68b6c1ba2348e7ae411d8dbd246731147
Michael Duncan <114211123+MacaDaca88@users.noreply.github.com>
2022-11-27 19:55:53 +1300

    Update door_sensor_pir.ino

--- door_sensor_pir.ino
+++ door_sensor_pir.ino
@@ -1,12 +1,12 @@
-
+#include <IRremote.h>
 /*
  * PIR sensor tester
  */
- 
+ int RECV_PIN = 10;
 int copblue = A2; // choose the pin for the LED
 int copred = A0;
 int naturalgreen = A1;
-int siren = 10;
+int siren = 9;
 int inputPin = 11;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
@@ -14,6 +14,8 @@ int brightness = 0;            // how bright the LED is starting
 int fadeAmount = 5;           // how many points to fade the LED by
 long randNumber;               // introduce randomizer
 
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 void setup() {
    Serial.begin(9600);
   pinMode(copred, OUTPUT);      // declare LED as output
@@ -60,5 +62,75 @@ analogWrite(naturalgreen, 255);
       pirState = LOW;
     }
   }
+  if (irrecv.decode(&results)) {
+      switch (results.value) {
+        case 0xFFA25D: Serial.println("CH-");
   
-}
+          break;
+        case 0xFFE21D: Serial.println("CH+");
+
+
+          break;
+
+        case 0xFF629D: Serial.println("CH");
+ 
+
+          break;
+        case 0xFF906F: Serial.println("EQ"); break;
+        case 0xFFE01F: Serial.println("-");
+          break;
+        case 0xFF22DD: Serial.println("PREVIOUS");
+ 
+
+        break;
+        case 0xFF02FD: Serial.println("NEXT"); 
+   
+
+        break;
+        case 0xFFC23D: Serial.println("PLAY");
+
+          break;
+        case 0xFFA857: Serial.println("+");
+
+
+          break;
+        case 0xFF6897: Serial.println("0");
+
+          break;
+        case 0xFF9867: Serial.println("100+");
+
+          break;
+        case 0xFFB04F: Serial.println("200+");
+
+          break;
+        case 0xFF30CF: Serial.println("1");
+
+          break;
+        case 0xFF18E7: Serial.println("2");
+   
+          break;
+        case 0xFF7A85: Serial.println("3");
+      
+          break;
+        case 0xFF10EF: Serial.println("4");
+      
+          break;
+        case 0xFF38C7: Serial.println("5");
+     
+          break;
+        case 0xFF5AA5: Serial.println("6");
+ 
+          break;
+        case 0xFF42BD: Serial.println("7");
+
+          break;
+        case 0xFF4AB5: Serial.println("8");
+  
+          break;
+        case 0xFF52AD: Serial.println("9");
+       
+      }
+      irrecv.resume();
+    }
+    delay(10);
+  }
