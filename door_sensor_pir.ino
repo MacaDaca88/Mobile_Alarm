50113dd3221f410689eb3ac17b877b7e9ae5fca0
MacaDaca88 <114211123+MacaDaca88@users.noreply.github.com>
2022-12-10 12:37:49 +1300

    Update door_sensor_pir.ino

--- door_sensor_pir.ino
+++ door_sensor_pir.ino
@@ -2,11 +2,11 @@
 /*
    PIR sensor tester
 */
-int RECV_PIN = 10;
+int RECV_PIN = 9;
 int blue = A2;                   // choose the pin for the blue LED
 int red = A0;                   // choose the pin for the red LED
 int green = A1;               // choose the pin for the green LED
-int siren = 9;
+int siren = 10;
 int inputPin = 11;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
@@ -25,92 +25,7 @@ void setup() {
   pinMode(siren, OUTPUT);
   pinMode(inputPin, INPUT);     // declare sensor as input
 }
-void ir() {
 
-  if (irrecv.decode(&results)) {
-    switch (results.value) {
-      case 0xFFA25D: Serial.println("CH-");
-        analogWrite(red, 255);
-        analogWrite(blue, 0);
-        analogWrite(green, 255);
-        break;
-      case 0xFFE21D: Serial.println("CH+");
-        analogWrite(red, 255);
-        analogWrite(blue, 0);
-        analogWrite(green, 0);
-        break;
-      case 0xFF629D: Serial.println("CH");
-
-        analogWrite(red, 255);
-        analogWrite(blue, 255);
-        analogWrite(green, 0);
-        delay(5000);
-        break;
-      case 0xFF906F: Serial.println("EQ");
-
-        break;
-      case 0xFFE01F: Serial.println("-");
-
-        break;
-      case 0xFF22DD: Serial.println("PREVIOUS");
-
-
-        break;
-      case 0xFF02FD: Serial.println("NEXT");
-
-
-        break;
-      case 0xFFC23D: Serial.println("PLAY");
-
-        break;
-      case 0xFFA857: Serial.println("+");
-
-
-        break;
-      case 0xFF6897: Serial.println("0");
-
-        break;
-      case 0xFF9867: Serial.println("100+");
-
-        break;
-      case 0xFFB04F: Serial.println("200+");
-
-        break;
-      case 0xFF30CF: Serial.println("1");
-
-        break;
-      case 0xFF18E7: Serial.println("2");
-
-        break;
-      case 0xFF7A85: Serial.println("3");
-
-        break;
-      case 0xFF10EF: Serial.println("4");
-
-        break;
-      case 0xFF38C7: Serial.println("5");
-
-        break;
-      case 0xFF5AA5: Serial.println("6");
-
-        break;
-      case 0xFF42BD: Serial.println("7");
-
-        break;
-      case 0xFF4AB5: Serial.println("8");
-
-        break;
-      case 0xFF52AD: Serial.println("9");
-
-    }
-    irrecv.resume();
-    analogWrite(red, 255);
-    analogWrite(blue, 255);
-    analogWrite(green, 255);
-  }
-  delay(10);
-
-}
 void loop() {
 
   val = digitalRead(inputPin);  // read input value
@@ -139,8 +54,11 @@ void loop() {
       pirState = HIGH;
     }
   } else {
-    ir();
-
+       digitalWrite(red, HIGH);
+        digitalWrite(green, HIGH);
+    digitalWrite(blue, HIGH);// turn LED ON
+    digitalWrite(siren, LOW);
+    
     if (pirState == HIGH) {
       // we have just turned of
       Serial.println("Motion ended!");
